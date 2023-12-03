34cf99120fa16401a995bd2e67c11ac20986c843
SpagOssssss <145158483+SpagOssssss@users.noreply.github.com>
2023-12-02 20:54:03 -0500

    Delete styles.js

--- styles.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const ongsLink = document.getElementById("ongs-link");
-    const ongsSection = document.getElementById("ongs");
-
-    ongsLink.addEventListener("click", function () {
-        ongsSection.classList.toggle("show");
-    });
-});
